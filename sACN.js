a8f846635c0cfd832588b3d3d66ae3454da96a77
Christian Lund <holmgaard1997@gmail.com>
2022-09-20 11:34:01 +0200

    A method broke the code. Now terminates after 60 seconds

--- sACN.js
+++ sACN.js
@@ -4,6 +4,7 @@ const sACN = new Receiver({
   universes: [1, 2],
   // see table 1 below for all options
 });
+console.log("Listening for sACN");
 
 sACN.on('packet', (packet) => {
   console.log('got dmx data:', packet.payload);
@@ -25,12 +26,14 @@ sACN.on('error', (err) => {
 });
 
 // start listening to a new universe (universe 3 in this example)
-sACN.addUniverse(3);
+// sACN.addUniverse(3);
 
 // stop listening to a universe 1
-sACN.removeUniverse(1);
+// sACN.removeUniverse(1);
 
 // close all connections; terminate the receiver
-sACN.close();
+setTimeout(() => {
+  sACN.close();
+}, 60000);
 
-sACN.universes; // is a list of the universes being listened to
+// sACN.universes; // is a list of the universes being listened to
